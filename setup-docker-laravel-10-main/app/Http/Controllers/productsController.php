bb690a2d8b03e5fa175251c5385c3228c909e9fc
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-09-05 20:11:25 -0300

    cria produto controller e a rota

---
+++ setup-docker-laravel-10-main/app/Http/Controllers/productsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productsController extends Controller
+
+//lembrete index() mais utilizada para paginação
+{
+    public function index() {
+        return 'teste';
+    }
+}
--- setup-docker-laravel-10-main/routes/web.php
+++ setup-docker-laravel-10-main/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\productsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::prefix('produtos')->group(function() {
+    Route::get('/', [productsController::class, 'index'])->name('home');
 });
