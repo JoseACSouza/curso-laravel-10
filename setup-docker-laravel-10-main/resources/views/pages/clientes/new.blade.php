dee099bc8bd03269d3d588c8c6fe1276b2078b5a
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-09-08 08:19:12 -0300

    implementa o autopreenchimento do endereco, lgoradouro e bairro baseado no cep via api

--- setup-docker-laravel-10-main/resources/views/pages/clientes/new.blade.php
+++ setup-docker-laravel-10-main/resources/views/pages/clientes/new.blade.php
@@ -21,28 +21,28 @@
   </div>
   <div class="col-auto mb-2">
     <label class="visually-hidden">Endereço</label>
-    <input type="text" value="{{ old('endereco') }}" class="form-control @error('endereco') is-invalid @enderror" placeholder="Endereço" name="endereco">
+    <input id="endereco" type="text" value="{{ old('endereco') }}" class="form-control @error('endereco') is-invalid @enderror" placeholder="Endereço" name="endereco">
     @if ($errors->has('endereco'))
       <div class="invalid-feedback"> {{$errors->first('endereço')}} </div>
     @endif
   </div>
   <div class="col-auto mb-2">
     <label class="visually-hidden">Logradouro</label>
-    <input type="text" value="{{ old('logradouro') }}" class="form-control @error('logradouro') is-invalid @enderror" placeholder="Logradouro" name="logradouro">
+    <input id="logradouro" type="text" value="{{ old('logradouro') }}" class="form-control @error('logradouro') is-invalid @enderror" placeholder="Logradouro" name="logradouro">
     @if ($errors->has('logradouro'))
       <div class="invalid-feedback"> {{$errors->first('logradouro')}} </div>
     @endif
   </div>
   <div class="col-auto mb-2">
     <label class="visually-hidden">CEP</label>
-    <input type="text" value="{{ old('cep') }}" class="form-control @error('cep') is-invalid @enderror" placeholder="CEP" name="cep">
+    <input id="cep" type="text" value="{{ old('cep') }}" class="form-control @error('cep') is-invalid @enderror" placeholder="CEP" name="cep">
     @if ($errors->has('cep'))
       <div class="invalid-feedback"> {{$errors->first('cep')}} </div>
     @endif
   </div>
   <div class="col-auto mb-2">
     <label class="visually-hidden">Bairro</label>
-    <input type="text" value="{{ old('bairro') }}" class="form-control @error('bairro') is-invalid @enderror" placeholder="Bairro" name="bairro">
+    <input id="bairro" type="text" value="{{ old('bairro') }}" class="form-control @error('bairro') is-invalid @enderror" placeholder="Bairro" name="bairro">
     @if ($errors->has('bairro'))
       <div class="invalid-feedback"> {{$errors->first('bairro')}} </div>
     @endif
